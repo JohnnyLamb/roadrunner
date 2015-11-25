1d275452567e7ee6ebd79098817545cef34df6fe
johnny lamb <agnusjack@gmail.com>
2015-11-25 14:44:17 -0700

    adding front end partials

--- src/client/main.js
+++ src/client/main.js
@@ -4,6 +4,14 @@ myApp.config(['$routeProvider', function ($routeProvider) {
   $routeProvider
     .when('/', {
       templateUrl: 'partials/home.html'
+    }).when('/login', {
+      templateUrl: 'partials/login.html',
+    })
+    .when('/logout', {
+    })
+    .when('/register', {
+      templateUrl: 'partials/register.html',
+
     })
     .otherwise({redirectTo: '/'});
 }]);

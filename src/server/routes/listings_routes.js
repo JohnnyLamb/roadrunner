f3616a96b1b0da2023f9188b8d1ff86f6647708f
johnny lamb <agnusjack@gmail.com>
2015-12-03 23:14:45 -0700

    got Twilio working and got most of the cron job working. need to work out logic for sraping to be checked in the cron job and then send the text message!

--- src/server/routes/listings_routes.js
+++ src/server/routes/listings_routes.js
@@ -3,6 +3,27 @@ var router = express.Router();
 var Listing = require('../models/listings.js');
 var mongoose = require('mongoose-q')(require('mongoose'));
 var User = require('../models/users.js');
+var CronJob = require('cron').CronJob;
+var text = require('textbelt');
+
+var client = require('twilio')('AC85148738df5f0791787140839270cec7', 'a6c87f9a3e0edfa9f41d30f3fd0e8b72');
+
+//Send an SMS text message
+
+
+
+
+
+// new CronJob('* 01 * * * *', function() {
+
+//   console.log('You will see this message when the cron job is done');
+// }, null, true, 'America/Denver');
+
+
+
+
+
+
 
 
 //////////////////////// SAVE SEARCH TEMPLATE FOR AREA TO A USER
@@ -11,6 +32,16 @@ var User = require('../models/users.js');
 
 router.post('/:userid/updateListings', function(req, res, next) {
   // console.log(req.body, ' this is the body');
+
+// client.sms.messages.post({
+//     to:'+17192383915',
+//     from:'+1 251-304-9672',
+//     body:'I love you baby! This is from my app!'
+// }, function(err, text) {
+//     console.log('You sent: '+ text.body);
+//     console.log('Current status of this text message is: '+ text.status);
+// });
+
 
   var toCheck = req.body.listingsArray[0];
   // var demoCheck = req.body.listingsArray[0];
